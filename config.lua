bf6b295f09c687a2a8be96920ca5fe47b19c6226
HeyyCzer <43618377+HeyyCzer@users.noreply.github.com>
2022-02-28 18:30:24 -0300

    Crianças configuradas

--- config.lua
+++ config.lua
@@ -1,17 +1,92 @@
 skins = {
     {
-		name = "Javali",
-        model = "a_c_boar",
-		image = "https://wiki.rage.mp/images/thumb/6/6f/Boar.png/101px-Boar.png",
+		name = "Criança #01",
+        model = "Baby-14",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947965686255345724/unknown.png",
 	},
     {
-		name = "Onça",
-        model = "a_c_mtlion",
-		image = "https://wiki.rage.mp/images/thumb/c/ce/MountainLion.png/96px-MountainLion.png",
+		name = "Criança #02",
+        model = "Baby-17",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947965779561824266/unknown.png",
 	},
     {
-		name = "Herói",
-        model = "u_m_y_imporage",
-		image = "https://wiki.rage.mp/images/thumb/6/69/ImpoRage.png/58px-ImpoRage.png",
+		name = "Criança #03",
+        model = "baby-28",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947965883043692564/unknown.png",
+	},
+    {
+		name = "Criança #04",
+        model = "Baby-bailarina",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947966006356242493/unknown.png",
+	},
+    {
+		name = "Criança #05",
+        model = "babyboy",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947966156361326632/unknown.png",
+	},
+    {
+		name = "Criança #06",
+        model = "Baby-girl75",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947966300666335272/unknown.png",
+	},
+    {
+		name = "Criança #07",
+        model = "baby-girl-panda",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947966380043558973/unknown.png",
+	},
+    {
+		name = "Criança #08",
+        model = "BabyMaite",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947966468954394664/unknown.png",
+	},
+    {
+		name = "Criança #09",
+        model = "Baby-girl-U",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947966617403420723/unknown.png",
+	},
+    {
+		name = "Criança #10",
+        model = "Baby-police",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947966731551404092/unknown.png",
+	},
+    {
+		name = "Criança #11",
+        model = "Baby-R-dragon",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947966894198104154/unknown.png",
+	},
+    {
+		name = "Criança #12",
+        model = "Baby-unicorn",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947967049592881182/unknown.png",
+	},
+    {
+		name = "Criança #13",
+        model = "JujubaCasual",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947967539923791902/unknown.png",
+	},
+    {
+		name = "Criança #14",
+        model = "laisbaby",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947967637013528616/unknown.png",
+	},
+    {
+		name = "Criança #15",
+        model = "nicolas",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947967713131765780/unknown.png",
+	},
+    {
+		name = "Criança #16",
+        model = "noahskin",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947967892073361448/unknown.png",
+	},
+    {
+		name = "Criança #17",
+        model = "SkinBebeMeninoNike",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947967966237061180/unknown.png",
+	},
+    {
+		name = "Criança #18",
+        model = "solskin",
+		image = "https://cdn.discordapp.com/attachments/947924235647590410/947968072214544414/unknown.png",
 	},
 }
